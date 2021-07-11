9ca9920271c8d5a342eb97421316722c5870bdef
natefduncan <natefduncan@gmail.coM>
2021-07-10 22:51:05 -0700

    test changes

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,14 @@ use rust_decimal_macros::dec;
 fn main() {
     // let lati: Decimal = dec!(32.78306);
     // let loni: Decimal = dec!(-96.80667);
-    let grid_size: Decimal = dec!(0.1); //KM
-    let radius: Decimal = dec!(4.0); //KM
+    let grid_size: Decimal = dec!(1); //KM
+    let radius: Decimal = dec!(5)   ; //KM
 
-    let points: Vec<density::Point> = utils::csv_file_to_Points("test.csv");
+    let points: Vec<density::Point> = utils::csv_file_to_Points("./test/mesa_church.csv");
+    let des: Vec<density::Point> = utils::csv_file_to_Points("./test/mesa_restaurant.csv");
     let undes: Vec<density::Point> = utils::csv_file_to_Points("test.csv");
-    let des: Vec<density::Point> = utils::csv_file_to_Points("test.csv");
-    let scores = density::score(points, undes, des, radius, grid_size);
-    println!("{:?}", scores);
-    //let circle: Vec<String> = density::calc_density(lati, loni, radius, grid_size);
-    //println!("{}", circle.len());
-    //utils::output_as_csv(circle);
+
+    println!("{}", points.len()); 
+    let scores = density::score(points, des, undes, radius, grid_size);
+    println!("{}", scores.len());
 }
