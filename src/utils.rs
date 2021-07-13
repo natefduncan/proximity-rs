0e52415312c4b45fe77d27674b2eb696c26bf6c9
natefduncan <natefduncan@gmail.coM>
2021-07-12 19:58:45 -0700

    support for stdin

--- src/utils.rs
+++ src/utils.rs
@@ -5,8 +5,18 @@ use rust_decimal_macros::dec;
 use std::collections::HashMap;
 use std::io;
 
-pub fn csv_file_to_Points(file_path: &str) -> Vec<Point> {
+pub fn csv_file_to_points(file_path: &str) -> Vec<Point> {
     let mut rdr = csv::Reader::from_path(&file_path).expect("Could not get from path.");
+    let mut output: Vec<Point> = Vec::new();
+    for result in rdr.deserialize() {
+        let record: Point = result.expect("Could not coerce to point.");
+        output.push(record);
+    }
+    return output;
+}
+
+pub fn stdin_to_points() -> Vec<Point> {
+    let mut rdr = csv::Reader::from_reader(io::stdin()); 
     let mut output: Vec<Point> = Vec::new();
     for result in rdr.deserialize() {
         let record: Point = result.expect("Could not coerce to point.");
